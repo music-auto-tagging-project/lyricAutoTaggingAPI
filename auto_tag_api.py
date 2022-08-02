e82ecb4660d391ecbb78edb195cccc01e9c5e531
moon <larcane@naver.com>
2022-08-02 07:53:23 +0000

    [update] remove all code related to sql
    - This API no longer needs to know DB information

--- auto_tag_api.py
+++ auto_tag_api.py
@@ -1,41 +1,35 @@
 import torch
 from src.model import KoSBERT,LyricAutoTagModel
-from src.utils import get_lyric_by_musicDB,save_tag_list_in_db
 from transformers import AutoModel,AutoTokenizer
-from flask import Flask, jsonify
+from flask import Flask, jsonify,request
 from flask_cors import CORS
 import argparse
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--host",type=str,help='db server endpoint')
-    parser.add_argument("--user",type=str,help='db login id')
-    parser.add_argument("--db",type=str,help='db name')
-    parser.add_argument("--password",type=str,help='db login password')
     parser.add_argument('--port',type=int,default=5000,help='port number to access from middleware or front')
+    parser.add_argument('--thresh',type=float,default=0.12,help='threshold for keyword')
+    parser.add_argument('--top_n',type=int,default=10,help='top-n for keyword')
     args = parser.parse_args()
     return args
 
 app = Flask (__name__)
 CORS(app)
 
-@app.route('/tag/<int:musicid>',methods=["GET"])
-def tag_extraction(musicid):
-    lyric = get_lyric_by_musicDB(musicid,host,user,db,password)
+@app.route('/music/tag/extraction',methods=["POST"])
+def tag_extraction():
+    lyric = request.get_json()['musicLyric']
     tag_list = auto_tag_model.get_keyword(lyric)
-    save_tag_list_in_db(tag_list,musicid,host,user,db,password)
     return jsonify({"tagList":tag_list})
 
 if __name__ == "__main__":
     args = parse_args()
-
-    host,user,db,password = args.host, args.user, args.db, args.password
 
     model = KoSBERT(
         AutoModel.from_pretrained('sentence-transformers/xlm-r-100langs-bert-base-nli-stsb-mean-tokens'),
         AutoTokenizer.from_pretrained('sentence-transformers/xlm-r-100langs-bert-base-nli-stsb-mean-tokens'),
         torch.device("cuda" if torch.cuda.is_available() else 'cpu')
     )
-    auto_tag_model = LyricAutoTagModel(model)
+    auto_tag_model = LyricAutoTagModel(model,top_n=args.top_n,sim_thresh=args.thresh)
 
     app.run(host='0.0.0.0',port=args.port)
--- src/utils.py
+++ src/utils.py
@@ -17,27 +17,3 @@ def eng_sent_tokenize(lyric):
         continue
       sents.append(s)
   return sents
-
-def get_lyric_by_musicDB(musicid,host,user,db,password):
-    db = pymysql.connect(host=host,user=user,db=db,password=password,charset='utf8',client_flag=CLIENT.MULTI_STATEMENTS)
-    with db:
-        with db.cursor() as cursor:
-            sql = """SELECT lyric FROM music WHERE id=%s"""
-            cursor.execute(sql,(musicid))
-            lyric = cursor.fetchone()[0]
-    return lyric
-
-def save_tag_list_in_db(tag_list,musicid,host,user,db,password):
-    db = pymysql.connect(host=host,user=user,db=db,password=password,charset='utf8',client_flag=CLIENT.MULTI_STATEMENTS)
-    with db:
-        with db.cursor() as cursor:
-            delete_prev_tag_sql = """DELETE FROM tag_has_music WHERE music_id=%s"""
-            cursor.execute(delete_prev_tag_sql,(musicid))
-            
-            tag_has_music_insert_sql = """INSERT INTO tag_has_music(tag_id,music_id,tag_rank)
-                                                VALUES((SELECT id FROM tag WHERE name=%s),%s,%s)"""
-            tag_insert_sql = """INSERT IGNORE INTO tag(name) VALUES(%s)"""
-            for rank,tag in enumerate(tag_list):
-                cursor.execute(tag_insert_sql,(tag))
-                cursor.execute(tag_has_music_insert_sql,(tag,musicid,rank+1))
-        db.commit()
