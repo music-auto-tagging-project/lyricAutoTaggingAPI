008f1632c1f536568f44567c9c007583a90e9b68
moon <larcane@naver.com>
2022-10-21 06:57:24 +0000

    [feat] add noun filtering process in auto tagging model - add korean noun dictionary - filtering auto-created tag if this don't be included in korean noun dictionary

--- auto_tag_api.py
+++ auto_tag_api.py
@@ -7,7 +7,6 @@ import argparse
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--port',type=int,default=5000,help='port number to access from middleware or front')
-    parser.add_argument('--thresh',type=float,default=0.12,help='threshold for keyword')
     parser.add_argument('--top_n',type=int,default=5,help='top-n for keyword')
     parser.add_argument("--model_name",type=str,default='sentence-transformers/xlm-r-100langs-bert-base-nli-stsb-mean-tokens')
     args = parser.parse_args()
@@ -18,13 +17,15 @@ CORS(app)
 
 @app.route('/music/tag/extraction',methods=["POST"])
 def tag_extraction():
-    lyric = request.get_json()['musicLyric']
-    result = auto_tag_model.get_keyword(lyric)
+    data = request.get_json()
+    lyric = data['musicLyric']
+    sim_thresh = data.get("simThresh",0.12)
+    result = auto_tag_model.get_keyword(lyric,sim_thresh)
     return jsonify(result)
 
 if __name__ == "__main__":
     args = parse_args()
 
-    auto_tag_model = LyricAutoTagModel(model_name=args.model_name,top_n=args.top_n,sim_thresh=args.thresh)
+    auto_tag_model = LyricAutoTagModel(model_name=args.model_name, top_n=args.top_n)
 
     app.run(host='0.0.0.0',port=args.port)
